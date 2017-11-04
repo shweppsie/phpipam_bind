7b3a2fb5ed3b6c7fcd0ffe6e3951ba5d68821820
Nathan Overall <git@shweppsie.com>
2017-11-04 18:45:30 +1300

    Rewite to new phpipam API
    Drop the external library dependency and just talk straight to the API

--- build_dns.py
+++ build_dns.py
@@ -12,48 +12,17 @@ path = os.path.dirname(sys.argv[0])
 
 from config import *
 
-p = phpipam.PHPIPAM(phpipam_url, phpipam_appid, phpipam_appcode)
-
-def check(r):
-    from pprint import pprint
-    if r['success'] != True:
-        print "An error occurred"
-        pprint(r)
-        exit(1)
-
-# load subnets
-r = json.loads(p.read_subnets())
-check(r)
-
-# find subnet we're looking for
-hosts = []
-for net_addr, net_size in nets:
-    subnet_id = None
-    for i in r['data']:
-        if i['subnet'] == net_addr and i['mask'] == net_size:
-            subnet_id = i['id']
-            break
-    if subnet_id == None:
-        print "An error occurred: Couldn't find %s/%s" % (net_addr, net_size)
-        exit(1)
-
-    # fetch ip addresses
-    ips = json.loads(p.read_addresses(subnet_id))
-    check(ips)
-
-    # print hosts
-    for i in ips['data']:
-        hosts.append((i['dns_name'], i['ip_addr']))
+hosts = phpipam.get_hosts()
 
 forward_output = open(os.path.join(path, 'templates/', forward_file)).read()
 reverse_output = open(os.path.join(path, 'templates/', reverse_file)).read()
 
-for h in hosts:
-    forward_name = h[0].split('.')[0]
-    forward_output += "%s IN A %s\n" % (forward_name, h[1])
+for host in hosts:
+    forward_name = host['hostname'].split('.')[0]
+    forward_output += "%s IN A %s\n" % (forward_name, host['ip'])
 
-    reverse_ip = '.'.join(reversed(h[1].replace('192.168.', '').split('.')))
-    reverse_name = h[0]
+    reverse_ip = '.'.join(reversed(host['ip'].replace('192.168.', '').split('.')))
+    reverse_name = host['hostname']
     if not reverse_name.endswith('.'):
         reverse_name += '.'
     reverse_output += "%s IN PTR %s\n" % (reverse_ip, reverse_name)
